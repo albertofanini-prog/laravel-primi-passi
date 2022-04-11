f0dd031e0f6cf5689be81e5ecc845eef1ce6394c
albertofanini-prog <albertofanini@outlook.com>
2022-04-11 16:26:11 +0200

    Inserimento dinamico messaggio

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $welcome = 'Hello world';
+    return view('home', compact('welcome'));
+})->name('home');
+
+Route::get('/info', function () {
     return view('home');
 });
